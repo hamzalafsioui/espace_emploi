92c35f156e599036190039a0634cd114b997ef39
hamzalafsioui <hamzalafsioui@gmail.com>
2026-01-27 12:23:31 +0100

    add table job_seekers

---
+++ database/migrations/2026_01_26_204124_create_job_seekers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_seekers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('specialty');
+            $table->string('experience_level')->nullable();
+            $table->text('skills')->nullable();
+            $table->string('cv_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_seekers');
+    }
+};
