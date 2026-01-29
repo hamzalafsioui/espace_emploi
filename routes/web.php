8003463a0c309e5830f03d71ea87b0774b369404
hamzalafsioui <hamzalafsioui@gmail.com>
2026-01-29 11:26:46 +0100

    Profile & Search & Friendships

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,20 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/profile/{user}', [ProfileController::class, 'show'])->name('profile.show');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-  
+    // Search
+    Route::get('/search', [SearchController::class, 'index'])->name('search.index');
+
+    // Friendships
+    Route::get('/friendships', [FriendshipController::class, 'index'])->name('friendships.index');
+    Route::post('/friendships', [FriendshipController::class, 'store'])->name('friendships.store');
+    Route::patch('/friendships/{friendship}', [FriendshipController::class, 'update'])->name('friendships.update');
+    Route::delete('/friendships/{friendship}', [FriendshipController::class, 'destroy'])->name('friendships.destroy');
 });
 
 require __DIR__ . '/auth.php';
