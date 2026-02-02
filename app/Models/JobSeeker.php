bd85a96d1af3071a174847172cf36081ac0972fb
hamzalafsioui <hamzalafsioui@gmail.com>
2026-02-02 12:10:50 +0100

    update Model Recruiter & jobSeeker to be match new Design

--- app/Models/JobSeeker.php
+++ app/Models/JobSeeker.php
@@ -10,12 +10,32 @@ class JobSeeker extends Model
         'user_id',
         'specialty',
         'experience_level',
-        'skills',
+        // 'skills', 
         'cv_path',
     ];
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function experiences()
+    {
+        return $this->hasMany(Experience::class);
+    }
+
+    public function educations()
+    {
+        return $this->hasMany(Education::class);
+    }
+
+    public function skills()
+    {
+        return $this->belongsToMany(Skill::class, 'job_seeker_skill')->withPivot('level');
+    }
+
+    public function applications()
+    {
+        return $this->hasMany(Application::class);
     }
 }
--- app/Models/Recruiter.php
+++ app/Models/Recruiter.php
@@ -19,4 +19,9 @@ class Recruiter extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function jobOffers()
+    {
+        return $this->hasMany(JobOffer::class);
+    }
 }
